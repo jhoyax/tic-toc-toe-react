1b52f67f55fb419f3ebc4348496a870d5358fde7
Joy Christine <joylopenaasis@gmail.com>
2019-07-13 22:14:21 +0800

    init

---
+++ precache-manifest.a1d6947ec5ad1bee0abdf630366d73b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fd49a98f90c429a7f7dd7b38389f9e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfbe7308ca5d4041c079",
+    "url": "/static/css/main.67d839ee.chunk.css"
+  },
+  {
+    "revision": "9a5c0cc5b7b5942488d9",
+    "url": "/static/js/2.56cb80cd.chunk.js"
+  },
+  {
+    "revision": "bfbe7308ca5d4041c079",
+    "url": "/static/js/main.0a77e9a2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
